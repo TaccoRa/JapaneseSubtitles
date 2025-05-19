045989bb7e985b856bc512030cd174b2b655623f
TaccoRa <169931185+TaccoRa@users.noreply.github.com>
2025-05-19 21:30:09 +0200

    Changed back to nonpyqt

--- src/SubtitlePlayer/main.py
+++ src/SubtitlePlayer/main.py
@@ -3,15 +3,8 @@
 # keep this for later information: to download subtitles:
 # https://kitsunekko.net/dirlist.php?dir=subtitles/japanese/One_Piece/&sort=date&order=asc
 
-import sys
-from PyQt5.QtWidgets import QApplication
-from app import App
-
-def main():
-    app = QApplication(sys.argv)
-    application = App()
-    application.run()
-    sys.exit(app.exec_())
+from app import SubtitlePlayerApp
 
 if __name__ == "__main__":
-    main()
+    app = SubtitlePlayerApp()
+    app.run()
